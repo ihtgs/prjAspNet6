258b484348172e20842ce5a0df1c098871b40eea
張正昱 <ihtgschen@gmail.com>
2022-09-05 21:09:53 +0800

    加入專案檔案。

---
+++ prjAspNet6/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjAspNet6.Data;
+using prjAspNet6.Models;
+
+namespace prjAspNet6.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> ohjCategoryList = _db.categories;
+            return View(ohjCategoryList);
+        }
+    }
+}
---
+++ prjAspNet6/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prjAspNet6.Models;
+
+namespace prjAspNet6.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
